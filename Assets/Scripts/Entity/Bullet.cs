2dfa04b0a44b227f2fdc0cb308fbd536f3e2787f
Kiattipoom Rojvanakarn <kiattipoom.r@gmail.com>
2022-07-21 21:35:57 +0700

    :sparkles: feat: very prototype shooting mechanics

---
+++ Assets/Scripts/Entity/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Entity
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class Bullet : MonoBehaviour
+    {
+        private const float Lifetime = 3;
+
+        private void Awake()
+        {
+            Destroy(gameObject, Lifetime);
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/BulletSpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.InputSystem;
+using UnityEngine;
+
+namespace Player
+{
+    public class BulletSpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject bulletPrefab;
+        [SerializeField] private float shootingSpeed;
+
+        private void Update()
+        {
+            if (!Keyboard.current.spaceKey.wasPressedThisFrame) return;
+            
+            Transform thisTransform = transform;
+            GameObject bullet = Instantiate(bulletPrefab, thisTransform.position, thisTransform.rotation);
+            bullet.GetComponent<Rigidbody>().velocity = thisTransform.forward * shootingSpeed;
+        }
+    }
+}
