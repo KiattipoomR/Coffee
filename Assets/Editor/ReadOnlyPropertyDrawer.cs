162aaf7b17ee65363566e044c614ff58e3c46c55
Kiattipoom Rojvanakarn <kiattipoom.r@gmail.com>
2022-08-05 20:32:06 +0700

    :sparkles: feat: implement read-only attribute drawer for debugging purposes

---
+++ Assets/Editor/ReadOnlyPropertyDrawer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+using Utils;
+
+namespace Editor
+{
+    [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnlyPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property,
+            GUIContent label)
+        {
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = true;
+        }
+    }
+}
