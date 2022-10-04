6ba1d3614a89a1540dad3767c2227678ed8222dc
Kiattipoom Rojvanakarn <kiattipoom.r@gmail.com>
2022-10-04 20:03:12 +0700

    :sparkles: feat: prototype hotbar switching

--- Assets/Scripts/Player/PlayerEquippedItem.cs
+++ Assets/Scripts/Player/PlayerEquippedItem.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Player
 {
     public class PlayerEquippedItem : MonoBehaviour
     {
+        private int _currentHotBarIndex = 1;
+            
         private void OnEnable()
         {
             PlayerController.OnPlayerAimed += PerformAim;
@@ -13,7 +16,29 @@ namespace Player
         {
             PlayerController.OnPlayerAimed -= PerformAim;
         }
-        
+
+        private void Update()
+        {
+            if (Keyboard.current.digit1Key.wasPressedThisFrame) _currentHotBarIndex = 1;
+            if (Keyboard.current.digit2Key.wasPressedThisFrame) _currentHotBarIndex = 2;
+            if (Keyboard.current.digit3Key.wasPressedThisFrame) _currentHotBarIndex = 3;
+            if (Keyboard.current.digit4Key.wasPressedThisFrame) _currentHotBarIndex = 4;
+
+            if (Keyboard.current.qKey.wasPressedThisFrame)
+            {
+                _currentHotBarIndex -= 1;
+                if (_currentHotBarIndex < 1) _currentHotBarIndex = transform.childCount;
+            }
+            
+            if (Keyboard.current.eKey.wasPressedThisFrame)
+            {
+                _currentHotBarIndex += 1;
+                if (_currentHotBarIndex > transform.childCount) _currentHotBarIndex = 1;
+            }
+            
+            SelectEquippedItem();
+        }
+
         private void PerformAim(Vector2 mousePositionInWorld)
         {
             var currentPosition = transform.position;
@@ -21,6 +46,16 @@ namespace Player
             var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             angle = angle < 0 ? angle + 360 : angle;
             transform.rotation = Quaternion.Euler(0f, 90 - angle, 0f);
+        }
+
+        private void SelectEquippedItem()
+        {
+            var hotBarIndex = _currentHotBarIndex - 1;
+
+            for (var i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).gameObject.SetActive(i == hotBarIndex);
+            }
         }
     }
 }
